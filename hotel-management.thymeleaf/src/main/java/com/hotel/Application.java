2ac7f71b7050631372873e7eff6393dfbb29f044
baki-511 <johanbaki381@gmail.com>
2024-10-05 14:37:14 +0530

    Created Project & basic stuff

---
+++ hotel-management.thymeleaf/src/main/java/com/hotel/Application.java
@@ -0,0 +1,14 @@
+package com.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		System.out.println("Application is Running..");
+	}
+
+}
